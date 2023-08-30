24c7b183e3962f10e374a04f9e0d8c4399702d55
rdheadyii <rdheadyii@gmail.com>
2023-08-30 12:08:59 -0400

    caught issue where sql AS and FROM were not caps

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ function searchDB() {
 };
 
 function employeeTable() {
-    let sql = `SELECT e.id , e.first_name, e.last_name, r.title,  d.name as department, r.salary, CONCAT(m.first_name,' ',m.last_name) as manager from employee e 
+    let sql = `SELECT e.id , e.first_name, e.last_name, r.title,  d.name as department, r.salary, CONCAT(m.first_name,' ',m.last_name) AS manager FROM employee e 
     LEFT JOIN role r ON e.role_id = r.id
     LEFT JOIN department d ON r.department_id = d.id 
     LEFT JOIN employee m ON m.id = e.manager_id`;
@@ -105,7 +105,7 @@ function employeeTable() {
 };
 
 function deptTable() {
-    let sql = '';
+    let sql = `SELECT `;
 
     db.query(sql, function (err, res) {
         console.table(res);
